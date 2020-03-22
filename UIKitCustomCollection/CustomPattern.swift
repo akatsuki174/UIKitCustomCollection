f79f720dfb34efd9f6833004a1083a1cdfacde65
akatsuki174 <dev2015js@gmail.com>
2020-03-22 14:12:29 +0900

    #1 add custom pattern enum

---
+++ UIKitCustomCollection/CustomPattern.swift
@@ -0,0 +1,12 @@
+//
+//  CustomPattern.swift
+//  UIKitCustomCollection
+//
+//  Created by Akatsuki on 2020/03/22.
+//  Copyright © 2020 akatsuki. All rights reserved.
+//
+
+enum CustomPattern: String, CaseIterable {
+    case `switch`
+    case stepper
+}
