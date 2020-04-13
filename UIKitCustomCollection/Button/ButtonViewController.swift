ee6fff8b8001bc873c7160e4edf4d32e8fac97e7
akatsuki174 <dev2015js@gmail.com>
2020-04-13 15:13:23 +0900

    #9 show button properties

--- UIKitCustomCollection/Button/ButtonViewController.swift
+++ UIKitCustomCollection/Button/ButtonViewController.swift
@@ -10,8 +10,32 @@ import UIKit
 
 class ButtonViewController: UIViewController {
     
+    @IBOutlet weak var customTarget: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    
+    private let viewModel = ButtonViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 }
 
+extension ButtonViewController: UITableViewDataSource {
+ 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let property = viewModel.property(index: indexPath.row) else { return UITableViewCell() }
+        let cell = UITableViewCell()
+        cell.textLabel?.text = property.name
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.numberOfRows()
+    }
+}
+
+extension ButtonViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
---
+++ UIKitCustomCollection/Button/ButtonViewModel.swift
@@ -0,0 +1,38 @@
+//
+//  ButtonViewModel.swift
+//  UIKitCustomCollection
+//
+//  Created by Akatsuki on 2020/04/13.
+//  Copyright © 2020 akatsuki. All rights reserved.
+//
+
+import Foundation
+
+class ButtonViewModel {
+    private let properties = ButtonProperty.allCases
+    
+    func numberOfRows() -> Int {
+        return properties.count
+    }
+    
+    func property(index: Int) -> ButtonProperty? {
+        if index >= properties.count { return nil }
+        return properties[index]
+    }
+    
+    enum ButtonProperty: String, CaseIterable, PropertyEnumProtocol {
+        case backgroundColor
+        case border
+        case borderWidth
+        case radius
+        case radiusValue
+        case shadow
+        case shadowOffset
+        case shadowOpacity // 濃さ
+        case shadowRadius // ぼかし量
+        case tappedText
+        case tappedTextColor
+        case image
+        case backgroundImage
+    }
+}
